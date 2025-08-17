9b0ad0c3b8da1ddfafe2c50979ff647daaa52589
alvaroginer <aginer079j@gmail.com>
2025-08-17 19:15:28 +0200

    adding loginForm

---
+++ myVueTaskPlanner/src/components/forms/LoginForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <InputText id="" label="" placeholder=""></InputText>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import InputText from '../input/InputText.vue';
+
+type LoginFormErrors = {
+  email?: string | undefined;
+  password: string | undefined;
+};
+
+export default defineComponent({
+  name: 'LoginForm',
+  components: {
+    InputText,
+  },
+  data() {
+    return {
+      formData: {
+        email: '' as string,
+        password: '' as string,
+      },
+      formDataErrors: {
+        succes: false as boolean,
+        errors: {} as LoginFormErrors,
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('hola');
+    },
+  },
+});
+</script>
